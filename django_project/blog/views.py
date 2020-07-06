025a4aed5ad98e9685cf3ab62ee6eb8cb0f32cd0
NIFA Yassine <yassine.nifa@renault.com>
2020-07-06 22:54:03 +0200

    add new templates and some css codes

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,32 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 # Create your views here.
+#Data
+posts = [
+	{
+		'author': 'Yassine',
+		'title': 'Blog Post 1',
+		'content': 'First Post Content',
+		'date_posted': '06/07/2020'
+
+	},
+	{
+		'author': 'Amine',
+		'title': 'Blog Post 2',
+		'content': 'Second Post Content',
+		'date_posted': '07/07/2020'
+
+	}
+
+]
+
 def home(request):
-	return HttpResponse('<h1>Blog Home</h1>')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return HttpResponse('<h1>Blog About</h1> ')
+	return render(request, 'blog/about.html',{'title': 'About'})
+	#return HttpResponse('<h1>Blog About</h1> ')
+
