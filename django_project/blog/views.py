ac54bf211a9049d1b7288beeb6a04a4d16eac7ab
NIFA Yassine <yassine.nifa@renault.com>
2020-07-07 17:04:34 +0200

    manipulate database with django

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,28 +1,30 @@
 from django.shortcuts import render
+from .models import Post
 # from django.http import HttpResponse
 # Create your views here.
-#Data
-posts = [
-	{
-		'author': 'Yassine',
-		'title': 'Blog Post 1',
-		'content': 'First Post Content',
-		'date_posted': '06/07/2020'
+#Dummies Data
+# posts = [
+# 	{
+# 		'author': 'Yassine',
+# 		'title': 'Blog Post 1',
+# 		'content': 'First Post Content',
+# 		'date_posted': '06/07/2020'
 
-	},
-	{
-		'author': 'Amine',
-		'title': 'Blog Post 2',
-		'content': 'Second Post Content',
-		'date_posted': '07/07/2020'
+# 	},
+# 	{
+# 		'author': 'Amine',
+# 		'title': 'Blog Post 2',
+# 		'content': 'Second Post Content',
+# 		'date_posted': '07/07/2020'
 
-	}
+# 	}
 
-]
+# ]
 
 def home(request):
 	context = {
-		'posts': posts
+		# 'posts': posts
+		'posts': Post.objects.all()
 	}
 	return render(request, 'blog/home.html', context)
 
